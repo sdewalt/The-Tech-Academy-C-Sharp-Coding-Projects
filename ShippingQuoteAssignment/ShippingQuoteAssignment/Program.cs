7907477a9be462e661a1ffd30a54fd888a026a6b
sdewalt <50034810+sdewalt@users.noreply.github.com>
2019-07-18 14:56:30 -0700

    Add files via upload

---
+++ ShippingQuoteAssignment/ShippingQuoteAssignment/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingQuoteAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight:");
+            double packageWeight = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package width:");
+            double packageWidth = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height:");
+            double packageHeight = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length:");
+            double packageLength = Convert.ToDouble(Console.ReadLine());
+            
+             double packageDimensions = (packageWidth + packageHeight + packageLength);
+
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Your package is too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                return;
+            }
+
+            if (packageWidth + packageHeight + packageLength > 50)
+            {
+                Console.WriteLine("Your package is too big to be shipped via Package Express.");
+                Console.ReadLine();
+                return;
+            }
+            double totalQuote = packageDimensions * packageWeight / 100;
+                
+            
+            Console.WriteLine("Your estimated total for shipping this package is: $"+totalQuote);
+            Console.WriteLine("Thank you.");
+            Console.ReadLine();
+        }            
+    }
+}
---
+++ ShippingQuoteAssignment/ShippingQuoteAssignment/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ShippingQuoteAssignment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("ShippingQuoteAssignment")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("294e9551-d170-4476-91ad-aaa14f37b39b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
